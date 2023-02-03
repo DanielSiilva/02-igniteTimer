9ca2b7815c29b25f8f094ee1c8c501c69ac813ec
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-02 21:34:24 -0300

    Páginas e rotas - Componente: Header

--- ignite-temer/src/components/Header/index.tsx
+++ ignite-temer/src/components/Header/index.tsx
@@ -1,3 +1,8 @@
+import { HeaderContainer } from "./styled";
+
+import Logo from '../../assets/Logo.svg'
+import { NavLink } from "react-router-dom";
+import { Timer, Scroll } from "phosphor-react";
 
 
 
@@ -5,8 +10,20 @@ export function Header(){
 
 
     return (
-        <>
-            <h1> Header</h1>
-        </>
+        <HeaderContainer>
+            <span>
+                <img src={Logo}/>
+            </span>
+
+            <nav>
+                <NavLink to='/' title="Timer">
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to='/history' title="Histórico">
+                    <Scroll size={24} />
+                </NavLink>
+
+            </nav>
+        </HeaderContainer>
     )
 }
