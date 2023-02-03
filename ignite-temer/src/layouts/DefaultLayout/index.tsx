2fcf67cd521d1f3c3a35eb45b8f042feb7a7e5d6
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-02 21:25:08 -0300

    Páginas e rotas - Estilização do Layout

--- ignite-temer/src/layouts/DefaultLayout/index.tsx
+++ ignite-temer/src/layouts/DefaultLayout/index.tsx
@@ -1,15 +1,17 @@
 
 import {Outlet} from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+
+import { LayoutContainer } from './styled'
 
 export function DefaultLayout(){
 
     return (
-        <>
+        <LayoutContainer>
             <Header />
             <Outlet/> 
         
-        </>
+        </LayoutContainer>
     )
 
 }
