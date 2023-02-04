fd763025f0da22a549c4566f76ba4209266566c2
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-03 21:49:13 -0300

    feat: Páginas e rotas - Página: History

--- ignite-temer/src/pages/History/index.tsx
+++ ignite-temer/src/pages/History/index.tsx
@@ -1,8 +1,46 @@
+import { HistoryContainer, HistoryList } from './styled'
 
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
 
-export function History (){
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Duração</th>
+              <th>Status</th>
+            </tr>
+          </thead>
 
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
 
-
-    return <h1>History</h1>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
