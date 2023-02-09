6a8a44c2326af8ba863a7da64e51806867136f89
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-09 20:46:49 -0300

    Feat: Contexto no React - Formatação de data

--- ignite-temer/src/pages/History/index.tsx
+++ ignite-temer/src/pages/History/index.tsx
@@ -1,17 +1,16 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styled'
 
-
 export function History() {
-  
   const { cycles } = useContext(CyclesContext)
-  
+
   
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
-
       <HistoryList>
         <table>
           <thead>
@@ -28,16 +27,19 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
                     )}
-
                     {cycle.interruptedDate && (
                       <Status statusColor="red">Interrompido</Status>
                     )}
-
                     {!cycle.finishedDate && !cycle.interruptedDate && (
                       <Status statusColor="yellow">Em andamento</Status>
                     )}
